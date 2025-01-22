1e50e6fb01c15bd5f89bbb8e7c39ef7d82605eb7
HectorCosgalla <valcos98@hotmail.com>
2025-01-21 19:26:30 -0600

    about to break the page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,26 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-tabs v-model="tab" no-caps class="folder-selector text-white shadow-2">
+      <q-tab class="folder" name="mails" label="Mails" />
+      <q-tab name="alarms" label="Alarms" />
+      <q-tab name="movies" label="Movies" />
+    </q-tabs>
   </q-page>
 </template>
 
 <script setup>
-//
+import { ref } from 'vue'
+
+const tab = ref('mails')
 </script>
+
+<style>
+.folder-selector {
+  background-color: aqua;
+}
+
+.folder {
+  margin: 5% 10%;
+  background-color: red;
+}
+</style>
