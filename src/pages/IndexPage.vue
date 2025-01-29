1562cac5b932907185c21edd44b18b864b1a1902
HectorCosgalla <valcos98@hotmail.com>
2025-01-28 19:13:31 -0600

    folder design done!

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,18 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-tabs v-model="tab" no-caps class="folder-selector text-white shadow-2">
+  <q-page class="">
+    <q-tabs
+      v-model="tab"
+      align="left"
+      active-bg-color="yellow-8"
+      class="bg-yellow-7"
+      @click="selectedFolder()"
+      no-caps
+    >
       <q-tab class="folder" name="mails" label="Mails" />
-      <q-tab name="alarms" label="Alarms" />
-      <q-tab name="movies" label="Movies" />
+      <q-tab class="folder" name="alarms" label="Alarms" />
+      <q-tab class="folder" name="movies" label="Movies" />
     </q-tabs>
+    <div class="cube"></div>
   </q-page>
 </template>
 
@@ -14,13 +22,11 @@ import { ref } from 'vue'
 const tab = ref('mails')
 </script>
 
-<style>
-.folder-selector {
-  background-color: aqua;
-}
-
+<style lang="scss">
 .folder {
-  margin: 5% 10%;
-  background-color: red;
+  margin: $space-base * 0.5 $space-base * 0.5 0 $space-base * 0.5;
+  border-radius: 20% 20% 0 0;
+  box-shadow: $shadow-12;
+  padding: 0 $space-base * 3;
 }
 </style>
